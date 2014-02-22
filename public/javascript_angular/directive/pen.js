3dc62bd54c5589fe0c50964f9d1b25078dc6fd65
gaethug <cbak@naver.com>
2014-02-23 03:38:07 +0900

    에디터를 찾아서 그 머나먼 여정을 시작하자마자 조난 빡세게 끝내버렸다. 시바 pen editor를 사용ㅎ여고 다이렉티브로 연결하였다. 떙규

---
+++ public/javascript_angular/directive/pen.js
@@ -0,0 +1,43 @@
+underdoggApp.directive('penEditor', [ function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            var editor = null;
+            var options = {
+                editor: element[0],//document.body, // {DOM Element} [required]
+                class: 'pen', // {String} class of the editor,
+                debug: true, // {Boolean} false by default
+                textarea: '<textarea name="content"></textarea>', // fallback for old browsers
+                list: [
+                    'blockquote', 'h2', 'h3', 'p', 'insertorderedlist', 'insertunorderedlist',
+                    'indent', 'outdent', 'bold', 'italic', 'underline', 'createlink'
+                ],
+                stay: true
+            }
+
+            var setModelValue = function(){
+                scope.$apply(function() {
+                    ngModel.$setViewValue(editor.config.editor.innerHTML);
+
+                });
+            };
+            setTimeout(function(){
+                editor = new Pen(options);
+                console.log(editor);
+                editor.config.editor.innerHTML = ngModel.$viewValue || '';
+                editor.config.editor.addEventListener('keypress', function(e) {
+                    console.log('keypress');
+                    setModelValue();
+                });
+                editor.config.editor.addEventListener('paste', function(e) {
+                    console.log('paste');
+                    setModelValue();
+                });
+            },0);
+
+        }
+
+    };
+
+}]);
--- public/javascript_angular/underdogg.js
+++ public/javascript_angular/underdogg.js
@@ -13,10 +13,19 @@ underdoggApp.config(function ($routeProvider, $locationProvider) {
     $routeProvider.
         when('/', {templateUrl: '/fragment/home/main' })
         .when('/testMain', {templateUrl: '/fragment/test/main'})
+        .when('/testCreate', {templateUrl: '/fragment/test/create', controller:'testCtrl'})
         .otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('!');
 });
 underdoggApp.controller('mainCtrl',function($rootScope,$scope){
     console.log('asdasd');
+});
+underdoggApp.controller('testCtrl',function($rootScope,$scope){
+    console.log('asdasd');
+    $scope.content = "<h1><span>마우스</span>로 글자를 드래그 해보세요.</h1><h3>이것은 Medium wannabe editor입니다.&nbsp;</h3><div>이 녀석의 출처는 <a href='https://github.com/sofish/pen'>이곳</a> 입니다.</div><div>어찌되었건. <b>angularJS</b>에서 일반 <i>javascrpit</i> 라이브러리를 쓰려면 약간의 귀차니즘을 동원해야 합니다.&nbsp;</div><div><br></div><p><b>2-way binding</b> 규칙을 지켜줘야 하거든요. 그렇기 때문에 view, model 각각 변화를 캐치하고 있다가 둘중 하나가 변경되면 서로 동기화를 시켜줘야 합니다. 굉장히 지랄맞을 수 있는게 view가 변경되어 model에 view값을 넣어줬는데 또 model은 변화를 캐치해서 그 값을 다시 view에 넣는 미친 회전을 할 수 있거든요. 그런데 다행히. model은 최초 페이지 로딩시 단 한번만 넣어줍니다.&nbsp;<br></p><p>새벽에 헛소리작렬... 자야지..</p> ";
+
+    $scope.$watch('content',function(newVal){
+        console.log(newVal);
+    });
 });
