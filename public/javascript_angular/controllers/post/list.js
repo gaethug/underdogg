ff7cecd28d08058a1e323655d6b50f40382f23d9
gaethug <cbak@naver.com>
2014-03-30 16:24:57 +0900

    일단 답답하니까 기존 포스트라도 떙겨오자 페이징도 좀 호재

--- public/javascript_angular/controllers/post/list.js
+++ public/javascript_angular/controllers/post/list.js
@@ -10,10 +10,13 @@ underdoggApp.controller('postListCtrl',function($rootScope, $scope, $location,po
     $scope.pager = 0;
     $scope.perPage = 4;
     var getList = function(page, perPage){
+        $scope.busy = true;
         postRest.query({page:page / perPage, perpage : perPage},function(data){
             if(!!data){
                 $scope.posts = data.posts;
+                $scope.pager += $scope.perPage;
             }
+            $scope.busy = false;
         });
     };
     $scope.loadMore = function(){
